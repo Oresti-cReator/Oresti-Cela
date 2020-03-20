f3147e4affd7beb0c144ffeaa4e0625fe72b7d67
Oresti-cReator <62442419+Oresti-cReator@users.noreply.github.com>
2020-03-20 19:08:42 +0100

    Add files via upload

---
+++ Html/js/custom.js
@@ -0,0 +1,8 @@
+var emri=prompt("Si quheni?","")
+alert("Gezohem qe po ju takoj "+emri)
+document.write("Gezohem qe po ju takoj "+emri)
+
+if (name == '') {
+	alert("Ju lutem fut emrin.")
+	return false
+}
